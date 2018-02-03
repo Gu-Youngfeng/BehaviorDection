2f5f6a449bb772a91858c7cd072df6be4d906a5d
BUGFIGHTER_YF <yongfeng_gu@163.com>
2018-02-03 14:55:45 +0800

    Launcher Updation
    update Launcher.java

--- src/main/java/cstar/yongfeng/collect/Launcher.java
+++ src/main/java/cstar/yongfeng/collect/Launcher.java
@@ -18,24 +18,23 @@ public class Launcher {
 		 * */
 		String userProfile = "E:/workspaceee/BehaviorDection/src/main/resources/userProfile.txt";
 		
-		
-		List<String> lsPositivePaths = UserDivition.getUserByKeyWrods(userProfile, "Positive");
-		System.out.println("\n[positive size]----" + lsPositivePaths.size());
-		for(String path: lsPositivePaths){
-			System.out.println("[userZip]: " +path);
-		}
-		
-		List<String> lsNegativePaths = UserDivition.getUserByKeyWrods(userProfile, "Negative");
-		System.out.println("\n[Negative size]----" + lsNegativePaths.size());
-		for(String path: lsNegativePaths){
-			System.out.println("[userZip]: " + path);
-		}
-		
-		List<String> lsNeutralPaths = UserDivition.getUserByKeyWrods(userProfile, "Neutral");
-		System.out.println("\n[Neutral size]----" + lsNeutralPaths.size());
-		for(String path: lsNeutralPaths){
-			System.out.println("[userZip]: " + path);
-		}
+//		List<String> lsPositivePaths = UserDivition.getUserByKeyWrods(userProfile, "Positive");
+//		System.out.println("\n[positive size]----" + lsPositivePaths.size());
+//		for(String path: lsPositivePaths){
+//			System.out.println("[userZip]: " +path);
+//		}
+//		
+//		List<String> lsNegativePaths = UserDivition.getUserByKeyWrods(userProfile, "Negative");
+//		System.out.println("\n[Negative size]----" + lsNegativePaths.size());
+//		for(String path: lsNegativePaths){
+//			System.out.println("[userZip]: " + path);
+//		}
+//		
+//		List<String> lsNeutralPaths = UserDivition.getUserByKeyWrods(userProfile, "Neutral");
+//		System.out.println("\n[Neutral size]----" + lsNeutralPaths.size());
+//		for(String path: lsNeutralPaths){
+//			System.out.println("[userZip]: " + path);
+//		}
 		
 		/**STEP 2: Analyze the user debugging behaviors. We just print them in the Console Window.
 		 * 
@@ -55,12 +54,32 @@ public class Launcher {
 //			usager.showMetric();
 //		}
 		
-		UsageGetter usager = new UsageGetter("C:/MSR18Dataset/Events-170301-2/Events-170301-2/2016-05-10/1.zip");
-		usager.showMetric();
+//		UsageGetter usager = new UsageGetter("C:/MSR18Dataset/Events-170301-2/Events-170301-2/2016-05-10/1.zip");
+//		usager.showMetric();
 		
 //		System.out.println("[FREE]: " + Runtime.getRuntime().freeMemory()/(1024*1024));
 //		System.out.println("[TOTA]: " + Runtime.getRuntime().totalMemory()/(1024*1024));
 		
+		/**
+		 * STEP 1 & 2
+		 */
+		DoWorks(userProfile, "Positive"); // to get rid of the risk of memory leak.
+//		DoWorks(userProfile, "Negative");
+//		DoWorks(userProfile, "Neutral");
+		
+	}
+	
+	public static void DoWorks(String userProfile, String prefix){
+		List<String> lsPositivePaths = UserDivition.getUserByKeyWrods(userProfile, prefix);
+//		System.out.println("\n[positive size]----" + lsPositivePaths.size());
+//		for(String path: lsPositivePaths){
+//			System.out.println("[userZip]: " +path);
+//		}
+		for(String path: lsPositivePaths){
+			UsageGetter usager = new UsageGetter(path);
+			usager.showMetric();
+			break;
+		}
 	}
 
 }
